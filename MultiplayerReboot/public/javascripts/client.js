e11da1848e0e9085f191325237494135f311cd62
christopher-witcher <christopher.witcher@gmail.com>
2014-05-29 16:07:06 -0700

    Able to connect and send traffic

---
+++ MultiplayerReboot/public/javascripts/client.js
@@ -0,0 +1,30 @@
+﻿    //var webAddress = "http://siblingrivalry.azurewebsites.net";
+var webAddress = "http://127.0.0.1:1337";
+
+//var io = require('socket.io');
+var socket = io.connect(webAddress);
+
+
+    
+    window.onload = function () {
+        var status = document.getElementById("status");
+        var name = document.getElementById("nameBox");
+        var joinBtn = document.getElementById("join");
+        //Submit users name for game play
+        joinBtn.addEventListener('click', function (e) {
+            socket.name = name;
+            socket.emit('join', {name: name.value });
+        }, false);
+
+        socket.on('start', function (data) {
+            
+            status.innerHTML = "Welcome to Sibling Rivalry<br />"
+                         + "Please enter your name and click join";
+
+        });
+
+        socket.on('waiting', function (data) {
+            status.innerHTML = data.message;
+        });
+
+    };
